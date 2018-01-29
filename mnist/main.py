69bc97abef8487bb82c9acf70cf863e596eaf6d7
Ben Asaf <ToBenAsaf@Gmail.com>
2018-01-29 16:22:17 +0200

    Fixed MNIST

--- mnist/main.py
+++ mnist/main.py
@@ -2,16 +2,20 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 from time import time
-import shutil, os
 from sys import stdout
 
 
-numIter = 25000
-batchSize = 128
-validationSize = 4096
-log_freq = 25
+NUM_EPOCHS = 50000
+BATCH_SIZE = 128
+VALIDATION_SIZE = 4096
+LOG_FREQUENCY = 25
+
+INITIAL_LEARNING_RATE = 0.01
+DECAY_RATE = 0.1
+NUM_EPOCHS_PER_DECAY = 30
+
 train_data, validation_data, test_data = input_data.read_data_sets("MNIST_data", one_hot=True, reshape=False,
-																   validation_size=validationSize)
+																   validation_size=VALIDATION_SIZE)
 
 
 def build_model(images, bIsTraining):
@@ -22,9 +26,9 @@ def build_model(images, bIsTraining):
 		nn = tf.nn.relu(nn)
 
 		for i in range(3):
-			nn = tf.layers.average_pooling2d(nn, pool_size=5, strides=(1, 1), padding="valid")
 			nn = tf.layers.batch_normalization(nn, training=bIsTraining)
 			nn = tf.layers.conv2d(nn, filters=64, kernel_size=5, strides=(1, 1), padding="same")
+			nn = tf.layers.average_pooling2d(nn, pool_size=5, strides=(1, 1), padding="valid")
 			nn = tf.nn.relu(nn)
 
 		nn = tf.layers.batch_normalization(nn, training=bIsTraining)
@@ -48,7 +52,8 @@ def build_model(images, bIsTraining):
 
 
 def get_loss_op(labels, logits):
-	return tf.reduce_mean(loss_op = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels, logits=logits))
+	loss_op = tf.nn.softmax_cross_entropy_with_logits_v2(labels=labels, logits=logits)
+	return tf.reduce_mean(loss_op)
 
 
 def get_accuracy_op(labels, logits):
@@ -60,15 +65,16 @@ def get_accuracy_op(labels, logits):
 
 
 def get_train_op(loss_op, global_step):
-
-	lr = tf.train.exponential_decay(0.05, global_step, batchSize*350, 0.1, staircase=True)
-
+	num_batches_per_epoch = train_data.num_examples / BATCH_SIZE
+	decay_steps = int(num_batches_per_epoch * NUM_EPOCHS_PER_DECAY)
+	lr = tf.train.exponential_decay(INITIAL_LEARNING_RATE, global_step, decay_steps, DECAY_RATE, staircase=True)
+	# lr = tf.Print(lr, [lr], "Learning rate: ")
 	update_ops = tf.get_collection(tf.GraphKeys.UPDATE_OPS)
 	opt = tf.train.GradientDescentOptimizer(lr)
 	with tf.control_dependencies(update_ops):
 		grads = opt.compute_gradients(loss_op)
 		apply_gradient_op = opt.apply_gradients(grads, global_step=global_step)
-	return apply_gradient_op, lr
+	return apply_gradient_op
 
 
 def train():
@@ -81,37 +87,17 @@ def train():
 	logits = build_model(images, bIsTraining)
 
 	loss_op = get_loss_op(labels, logits)
-	train_op, LR = get_train_op(loss_op, global_step)
+	train_op = get_train_op(loss_op, global_step)
 	accuracy_op = get_accuracy_op(labels, logits)
 
 	scaffold = tf.train.Scaffold(init_op=tf.global_variables_initializer(),
 								 local_init_op=tf.local_variables_initializer())
 
-	# saver = tf.train.Saver(max_to_keep=1)
-
-	# with tf.Session() as sess:
-	# 	sess.run(tf.global_variables_initializer())
-	# 	print("%10s | %10s | %12s | %8s" % ("Iteration", 'Samples', 'Train Loss', 'Accuracy'))
-	# 	highest_acc = 0.0
-	# 	t_losses = np.array([])
-	# 	for i in range(numIter+1):
-	# 		X, Y = mnist.train.next_batch(batch_size=batchSize)
-	# 		_, t_loss = sess.run([train_op, loss_op], feed_dict={images: X, labels: Y, bIsTraining: True})
-	# 		t_losses = np.append(t_losses, t_loss)
-	# 		if i % 50 == 0:
-	# 			X_valid, Y_valid = mnist.validation.next_batch(validationSize)
-	# 			acc = accuracy_op.eval(feed_dict={images: X_valid, labels: Y_valid, bIsTraining: False})
-	# 			print("%10d | %10d | %12.8f | %.3f%% | %1.6f" % (i, i * batchSize, np.mean(t_losses), acc, sess.run(LR)))
-	# 			t_losses = np.array([])
-	# 			if highest_acc < acc:
-	# 				highest_acc = acc
-	# 				saver.save(sess, "./model/model.chkpt", global_step=global_step)
 	class _LoggerHook(tf.train.SessionRunHook):
 		"""Logs loss and runtime."""
 
 		def __init__(self, *args, **kwargs):
 			super(*args, **kwargs)
-			self._step = -1
 			self._start_time = time()
 			self._train_losses = np.array([], dtype=np.float32)
 			self._train_accuracies = np.array([], dtype=np.float32)
@@ -120,40 +106,37 @@ def train():
 			print("%8s | %9s | %11s | %10s | %5s%% | %10s%%" % ("Duration", "Epochs", 'Samples', 'Loss', 'Train', 'Validation'))
 
 		def before_run(self, run_context):
-			self._step += 1
 			return tf.train.SessionRunArgs([loss_op, accuracy_op])
 
 		def after_run(self, run_context, run_values):
 			loss_value, accuracy_value = run_values.results
 			self._train_losses = np.append(self._train_losses, loss_value)
 			self._train_accuracies = np.append(self._train_accuracies, accuracy_value)
-			if self._step % log_freq == 0:
+			sess = run_context.session
+			step = sess.run(global_step)
+			if step % LOG_FREQUENCY == 0:
 				current_time = time()
 				duration = current_time - self._start_time
 				self._start_time = current_time
 
-				X, Y = validation_data.next_batch(batchSize)
-				avg_validation_acc = run_context.session.run(accuracy_op,
-															 feed_dict={images: X, labels: Y, bIsTraining: False})
+				X, Y = validation_data.next_batch(BATCH_SIZE)
+				avg_validation_acc = sess.run(accuracy_op, feed_dict={images: X, labels: Y, bIsTraining: False})
 				avg_train_loss = np.mean(self._train_losses, dtype=np.float32)
 				avg_train_acc = np.mean(self._train_accuracies, dtype=np.float32)
-				print("%3.2f sec | %6d | %11d | %10.6f | %5.2f%% | %5.2f%%" % (duration, self._step, self._step*batchSize,
-																		  avg_train_loss, avg_train_acc,
-																		  avg_validation_acc))
+				print("%3.2f sec | %6d | %11d | %10.6f | %5.2f%% | %5.2f%%" % (duration, step, step * BATCH_SIZE,
+																			   avg_train_loss, avg_train_acc,
+																			   avg_validation_acc))
 				self._train_losses, self._train_accuracies = np.array([]), np.array([])
 
 	with tf.train.MonitoredTrainingSession(checkpoint_dir="./model/",
 			scaffold=scaffold,
-			hooks=[tf.train.StopAtStepHook(last_step=numIter),
+			hooks=[tf.train.StopAtStepHook(last_step=NUM_EPOCHS),
 				   tf.train.NanTensorHook(loss_op),
 				   _LoggerHook()]) as mon_sess:
 		while not mon_sess.should_stop():
-			X, Y = train_data.next_batch(batchSize)
+			X, Y = train_data.next_batch(BATCH_SIZE)
 			mon_sess.run(train_op, feed_dict={images: X, labels: Y, bIsTraining: True})
 
-		# X, Y = mnist.train.next_batch(batch_size=1)
-		# ytag = sess.run(prediction, feed_dict={x: X})
-		# print(ytag, Y[0])
 
 
 def load_and_test():
@@ -180,15 +163,13 @@ def load_and_test():
 		for i in range(NUM_TEST_IMGS):
 			stdout.write("\rPredicting %d/%d" % (i+1, NUM_TEST_IMGS))
 			X, Y = test_data.next_batch(1)
-			sess.run(predict_op, feed_dict={images: X, labels:Y, bIsTraining: False})
+			sess.run(predict_op, feed_dict={images: X, labels: Y, bIsTraining: False})
 		stdout.write("\n")
 		num_correct = sess.run(total_correct_predictions)
 	print("Accuracy: %3.3f%%" % (float(num_correct/NUM_TEST_IMGS)*100))
 
 
 def main():
-	# if os.path.exists("./model/"):
-	# 	shutil.rmtree("./model/")
 	train()
 	load_and_test()
 
